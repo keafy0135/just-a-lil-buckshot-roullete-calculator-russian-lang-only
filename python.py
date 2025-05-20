29cb443e9162dddb080341cbc6d8c12a16216fea
Keafy <oleg.yudin.11@mail.ru>
2025-05-20 20:08:33 +0300

    added english
    added english, made better input(now u can type the full word)

--- python.py
+++ python.py
@@ -1,22 +1,23 @@
 j = 0
 k = 0
 def check_bullets(f, r):
-    f = int(input("Сколько боевых? "))
-    r = int(input("сколько холостых? "))
+    f = int(input("Сколько боевых?\what amount of armed bullets "))
+    r = int(input("сколько холостых?what amount of unarmed bullets "))
     return f, r
 
 j, k = check_bullets(j, k)
 z = j + k
 while z != 0:
     t = input("Какой выпал патрон? ")
-    if t == "r":
+    t=t.lower()
+    if t == "боевой" or t=='armed':
         j -= 1
-        print(f"боевых {j}, холостых {k}")
+        print(f"боевых(armed) {j}, холостых(unarmed) {k}")
         z = j + k
-    if t == "f":
+    if t == 'unarmed' or t=='холостой':
         k -= 1
-        print(f"боевых {j}, холостых {k}")
+        print(f"боевых(armed) {j}, холостых(unarmed) {k}")
         z = j + k
-    if t == "нв":   
+    if t == 'нв' or t=='new game' or t=='новая игра' or t=='ng':   
         j, k = check_bullets(j, k)
         z = j + k
